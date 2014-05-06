d157d9fb03e737df04b8720fa4ef809e9d46f251
Marc Brooks <IDisposable@gmail.com>
2014-05-06 17:20:00 -0500

    Gaining momentum.

---
+++ Taxation.js
@@ -0,0 +1,4 @@
+var compounding = document.createElement('script');
+compounding.setAttribute('type','text/javascript');
+compounding.setAttribute('src','Taxation.js');
+document.head.appendChild(compounding);
